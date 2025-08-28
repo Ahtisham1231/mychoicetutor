232aca4f1f40d4f0ca4ac2dabb23fed34d58ab81
Ahtisham1232 <ahishamullhaq1@gmail.com>
2025-08-28 23:50:39 +0500

    resourse issue resloved

--- resources/views/front-cms/tutordetails.blade.php
+++ resources/views/front-cms/tutordetails.blade.php
@@ -8,7 +8,7 @@
             <div class="row">
 
                 <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 profileSec1">
-                    <h2>{{ $tutorpd->headline }}</h2>
+                    <h2>{{ $tutorpd->headline ?? '' }}</h2>
 
                     <h6 class="mb-2">Subjects</h6>
 
@@ -21,21 +21,21 @@
 
                     <div class="aboutTutor">
 
-                        <h5>About {{ $tutorpd->name }}</h5>
-                        <p class="charcol">{{ $tutorpd->goal }}</p>
-                        <p class="charcol">{{ $tutorpd->detail_1 }}</p>
+                        <h5>About {{ $tutorpd->name  ?? '' }}</h5>
+                        <p class="charcol">{{ $tutorpd->goal  ?? '' }}</p>
+                        <p class="charcol">{{ $tutorpd->detail_1  ?? '' }}</p>
 
                     </div>
                     <div class="aboutTutor">
 
                         <h5>Qualification</h5>
-                        <p class="charcol">{{ $tutorpd->qualification }}</p>
+                        <p class="charcol">{{ $tutorpd->qualification  ?? '' }}</p>
 
                     </div>
                     <div class="aboutTutor">
 
                         <h5>Certification</h5>
-                        <p class="charcol">{{ $tutorpd->certification }}</p>
+                        <p class="charcol">{{ $tutorpd->certification  ?? '' }}</p>
 
                     </div>
 
@@ -128,7 +128,7 @@
                         </div>
                         <div class="col-lg-12 col-md-7">
 
-                            <p class="name mt-3">{{ $tutorpd->name }}</p>
+                            <p class="name mt-3">{{ $tutorpd->name  ?? '' }}</p>
                             <div class="star">
                                 <span>
                                     {{-- Loop to display the average number of stars --}}
@@ -155,11 +155,11 @@
 
                                 <tr>
                                     <td>Hourly rate:</td>
-                                    <td>£{{ $tutorpd->rateperhour }}</td>
+                                    <td>£{{ $tutorpd->rateperhour  ?? '' }}</td>
                                 </tr>
                                 <tr>
                                     <td>Experience:</td>
-                                    <td>{{ $tutorpd->experience }}</td>
+                                    <td>{{ $tutorpd->experience  ?? '' }}</td>
                                 </tr>
 
                             </table>
@@ -189,7 +189,7 @@
 
     <div class="container tutor-card">
         <div class="aboutTutor">
-            <h5 class="my-5">Other tutors in {{ $primarysubjects->subject_name }}</h5>
+            <h5 class="my-5">Other tutors in {{ $primarysubjects->subject_name  ?? '' }}</h5>
         </div>
         <div class="row">
             @foreach ($othertutors->slice(0, 4) as $othertutor)
@@ -204,13 +204,13 @@
                             <div class="star">
                                 <span>
                                     <i class="fa fa-star"></i>
-                                   {{$othertutor->avg_rating}} ({{$othertutor->total_reviews}})
+                                   {{$othertutor->avg_rating}} ({{$othertutor->total_reviews  ?? '' }})
                                 </span>
-                                <span>£{{ $othertutor->rateperhour }}/h</span>
+                                <span>£{{ $othertutor->rateperhour  ?? '' }}/h</span>
                             </div>
                             <a href="/findatutor" style="color: black"> <span class="name">
                                     {{ $othertutor->name }}
-                                    <p>{{ $primarysubjects->subject_name }} teacher</p>
+                                    <p>{{ $primarysubjects->subject_name  ?? '' }} teacher</p>
                                 </span></a>
 
                         </div>
